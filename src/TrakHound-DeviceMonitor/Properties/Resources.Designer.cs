1c29d3fff80cdbcd19fc0c48e0c37f6b3d5f7de4
Patrick Ritchie <Patrick@TrakHound.com>
2017-04-06 02:45:01 -0400

    Updated Icons and Nuget packages

--- src/TrakHound-DeviceMonitor/Properties/Resources.Designer.cs
+++ src/TrakHound-DeviceMonitor/Properties/Resources.Designer.cs
@@ -131,6 +131,26 @@ namespace TrakHound.DeviceMonitor.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon devicemonitor_logo {
+            get {
+                object obj = ResourceManager.GetObject("devicemonitor_logo", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap devicemonitor_logo_100px {
+            get {
+                object obj = ResourceManager.GetObject("devicemonitor_logo_100px", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap Dock_01 {
